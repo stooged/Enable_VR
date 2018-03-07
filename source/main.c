4d9fc986ce60ec2dc6d45f8cf14d570698c77ca5
Stooged <stooged@users.noreply.github.com>
2018-03-07 23:51:06 +0800

    Initial

---
+++ source/main.c
@@ -0,0 +1,147 @@
+
+#include "ps4.h"
+
+
+
+unsigned int long long __readmsr(unsigned long __register) {
+	unsigned long __edx;
+	unsigned long __eax;
+	__asm__ ("rdmsr" : "=d"(__edx), "=a"(__eax) : "c"(__register));
+	return (((unsigned int long long)__edx) << 32) | (unsigned int long long)__eax;
+}
+
+#define X86_CR0_WP (1 << 16)
+
+static inline __attribute__((always_inline)) uint64_t readCr0(void) {
+	uint64_t cr0;
+	
+	asm volatile (
+		"movq %0, %%cr0"
+		: "=r" (cr0)
+		: : "memory"
+ 	);
+	
+	return cr0;
+}
+
+static inline __attribute__((always_inline)) void writeCr0(uint64_t cr0) {
+	asm volatile (
+		"movq %%cr0, %0"
+		: : "r" (cr0)
+		: "memory"
+	);
+}
+
+
+
+struct auditinfo_addr {
+    char useless[184];
+};
+
+struct ucred {
+	uint32_t useless1;
+	uint32_t cr_uid;     // effective user id
+	uint32_t cr_ruid;    // real user id
+ 	uint32_t useless2;
+    	uint32_t useless3;
+    	uint32_t cr_rgid;    // real group id
+    	uint32_t useless4;
+    	void *useless5;
+    	void *useless6;
+    	void *cr_prison;     // jail(2)
+    	void *useless7;
+    	uint32_t useless8;
+    	void *useless9[2];
+    	void *useless10;
+    	struct auditinfo_addr useless11;
+    	uint32_t *cr_groups; // groups
+    	uint32_t useless12;
+};
+
+struct filedesc {
+	void *useless1[3];
+    	void *fd_rdir;
+    	void *fd_jdir;
+};
+
+struct proc {
+    	char useless[64];
+    	struct ucred *p_ucred;
+    	struct filedesc *p_fd;
+};
+
+struct thread {
+    	void *useless;
+    	struct proc *td_proc;
+};
+
+
+int patchver(struct thread *td){
+
+	struct ucred* cred;
+	struct filedesc* fd;
+
+	fd = td->td_proc->p_fd;
+	cred = td->td_proc->p_ucred;
+
+	void* kernel_base = &((uint8_t*)__readmsr(0xC0000082))[-0x3095D0];
+	uint8_t* kernel_ptr = (uint8_t*)kernel_base;
+	void** got_prison0 =   (void**)&kernel_ptr[0x10399B0];
+	void** got_rootvnode = (void**)&kernel_ptr[0x21AFA30];
+
+	cred->cr_uid = 0;
+	cred->cr_ruid = 0;
+	cred->cr_rgid = 0;
+	cred->cr_groups[0] = 0;
+
+	cred->cr_prison = *got_prison0;
+	fd->fd_rdir = fd->fd_jdir = *got_rootvnode;
+
+	// escalate ucred privs, needed for access to the filesystem ie* mounting & decrypting files
+	void *td_ucred = *(void **)(((char *)td) + 304); // p_ucred == td_ucred
+	
+	// sceSblACMgrIsSystemUcred
+	uint64_t *sonyCred = (uint64_t *)(((char *)td_ucred) + 96);
+	*sonyCred = 0xffffffffffffffff;
+	
+	// sceSblACMgrGetDeviceAccessType
+	uint64_t *sceProcType = (uint64_t *)(((char *)td_ucred) + 88);
+	*sceProcType = 0x3801000000000013; // Max access
+	
+	// sceSblACMgrHasSceProcessCapability
+	uint64_t *sceProcCap = (uint64_t *)(((char *)td_ucred) + 104);
+	*sceProcCap = 0xffffffffffffffff; // Sce Process
+
+	// Disable write protection
+	uint64_t cr0 = readCr0();
+	writeCr0(cr0 & ~X86_CR0_WP);
+
+    //fw 5+  4.55 offset
+    *(uint32_t *)(kernel_base + 0x144B600) = 0x5050001;
+
+	// Restore write protection
+	writeCr0(cr0);
+
+	return 0;
+}
+
+
+double(*ceil)(double x);
+int(*sceSysUtilSendSystemNotificationWithText)(int messageType, char* message);
+
+void systemMessage(char* msg) {
+	sceSysUtilSendSystemNotificationWithText(222, msg);
+}
+
+
+int _main(struct thread *td) {
+	initKernel();
+	initLibc();
+        syscall(11,patchver,td);
+        int sysUtil = sceKernelLoadStartModule("/system/common/lib/libSceSysUtil.sprx", 0, NULL, 0, 0, 0);
+        RESOLVE(sysUtil, sceSysUtilSendSystemNotificationWithText);
+        systemMessage("Start VR Headset.");
+        return 0;
+}
+
+
